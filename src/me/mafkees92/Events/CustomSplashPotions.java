ed49e4d98d601a89b40076ce76b30b1afe2a7b64
Mafkees92 <unconfigured@null.spigotmc.org>
2020-07-27 17:35:55 +0200

    updated splashpotion code

--- src/me/mafkees92/Events/CustomSplashPotions.java
+++ src/me/mafkees92/Events/CustomSplashPotions.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -11,75 +12,85 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-
 import me.mafkees92.Main;
+import me.mafkees92.Utils.Utils;
 
-public class CustomSplashPotions implements Listener{
-	
+public class CustomSplashPotions implements Listener {
+
 	private final Plugin plugin;
+
 	public CustomSplashPotions(Main main) {
-	     this.plugin = main;
+		this.plugin = main;
 	}
 
-	final String potionName = "Freeze";
-	final String loreLineOne = "Freeze your enemies";
-	
+	final String CustomPotionNBTKey = "customPotions";
+	final String freezePotionNBTTag = "freezePotion";
+	ArrayList<Block> changedBlocks = new ArrayList<Block>();
+
 	@EventHandler
 	public void onSplashPotion(PotionSplashEvent e) {
-		
+
 		ThrownPotion potion = e.getPotion();
 		ItemStack item = potion.getItem();
-		
-		if(item.hasItemMeta()) {
-			if(item.getItemMeta().getDisplayName().equals(potionName)) {
-				if(item.getItemMeta().getLore().contains(loreLineOne)){
-					e.setCancelled(true);
-					PotionEffect effect = new PotionEffect(PotionEffectType.SLOW, 70, 10);
-					for (LivingEntity entity : e.getAffectedEntities()) {
-						entity.addPotionEffect(effect);
-					}
-					setBlocks(e.getEntity().getLocation());
-				}
+
+		String nbtTag = Utils.getNBTTag(item, CustomPotionNBTKey);
+		if (nbtTag.equals(freezePotionNBTTag)) {
+			e.setCancelled(true);
+			PotionEffect effect = new PotionEffect(PotionEffectType.SLOW, 80, 10);
+			for (LivingEntity entity : e.getAffectedEntities()) {
+				entity.addPotionEffect(effect);
 			}
+			setBlocks(e.getEntity().getLocation());
+
 		}
 	}
-	
+
+	@EventHandler
+	public void onIceMelting(BlockFadeEvent e) {
+		if (changedBlocks.contains(e.getBlock())) {
+			e.setCancelled(true);
+		}
+	}
+
 	private void setBlocks(Location location) {
-	
-		ArrayList<Block> changedBlocks = new ArrayList<Block>();
-		int radius = 4 ;
+		ArrayList<Block> changed = new ArrayList<Block>();
+		int radius = 4;
 		Block block = location.getBlock();
-		for (int x = -(radius); x <= radius; x ++) {
-			for (int z = -(radius); z <= radius; z ++) {
-				Block relativeBlock = block.getRelative(x,0,z);
-				if(relativeBlock.getType().equals(Material.AIR)) {
+		for (int x = -(radius); x <= radius; x++) {
+			for (int z = -(radius); z <= radius; z++) {
+				Block relativeBlock = block.getRelative(x, 0, z);
+				if (relativeBlock.getType().equals(Material.AIR)) {
 					Random random = new Random();
 					int randInt = random.nextInt(100);
-					if(randInt < 20) {
+					if (randInt < 20) {
 						relativeBlock.setType(Material.PACKED_ICE);
-						changedBlocks.add(relativeBlock);
-					}
-					else if(randInt < 50) {
+						changed.add(relativeBlock);
+						this.changedBlocks.add(relativeBlock);
+					} else if (randInt < 50) {
 						relativeBlock.setType(Material.ICE);
-						changedBlocks.add(relativeBlock);
+						changed.add(relativeBlock);
+						this.changedBlocks.add(relativeBlock);
 					}
 				}
 			}
 		}
-		Bukkit.getScheduler().runTaskLater(plugin, () -> removeBlocks(changedBlocks), 70);
+		Bukkit.getScheduler().runTaskLater(plugin, () -> removeBlocks(changed), 80);
 	}
-	
-	
+
 	private void removeBlocks(ArrayList<Block> blocksToRemove) {
-	
+
 		for (Block block : blocksToRemove) {
+			if (block.getType().equals(Material.PACKED_ICE))
+				block.getWorld().playEffect(block.getLocation(), Effect.SMOKE, 4, 50);
 			block.setType(Material.AIR);
+			this.changedBlocks.remove(block);
 		}
 	}
 
