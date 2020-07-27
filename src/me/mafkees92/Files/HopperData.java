91bbcac5e15561405079acac7c667a7b7519c323
Mafkees92 <unconfigured@null.spigotmc.org>
2020-07-27 17:37:26 +0200

    Create HopperData.java

---
+++ src/me/mafkees92/Files/HopperData.java
@@ -0,0 +1,24 @@
+package me.mafkees92.Files;
+
+import org.bukkit.block.Hopper;
+
+import me.mafkees92.Main;
+
+public class HopperData extends BaseFile {
+
+	
+	
+	
+	
+	public HopperData(Main main) {
+		super(main, "hopperdata.yml");
+	}
+
+	public void addHopper(Hopper hopper) {
+		//Location loc = new Location(world, x, y, z)
+
+	}
+	
+	
+	
+}
