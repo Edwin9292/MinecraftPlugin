c6d2bae753760c8568dca2fde27eb518c31d3153
Mafkees92 <unconfigured@null.spigotmc.org>
2020-07-28 23:45:47 +0200

    added placeholders for islands size, average island rating, player health

--- src/me/mafkees92/MVdWPlaceholders/mvdwPlaceholders.java
+++ src/me/mafkees92/MVdWPlaceholders/mvdwPlaceholders.java
@@ -7,7 +7,9 @@ public class mvdwPlaceholders {
 	public mvdwPlaceholders(Main plugin){
 
 		//register all custom placeholders here
-		new askyblock_island_size(plugin);
+		new mafkees_island_size(plugin);
+		new mafkees_average_island_rate(plugin);
+		new mafkees_player_health(plugin);
 		
 	}
 	
