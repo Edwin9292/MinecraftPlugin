dfd03eafc5b46728d9b6d8e6db0e01a3e30119be
Mafkees92 <unconfigured@null.spigotmc.org>
2020-07-30 12:04:55 +0200

    Update for placeholder to check if player has an island, else use the rating of team owners island

--- src/me/mafkees92/MVdWPlaceholders/mafkees_average_island_rate.java
+++ src/me/mafkees92/MVdWPlaceholders/mafkees_average_island_rate.java
@@ -1,6 +1,11 @@
 package me.mafkees92.MVdWPlaceholders;
 
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import com.wasteofplastic.askyblock.ASkyBlockAPI;
 
 import be.maximvdw.placeholderapi.PlaceholderAPI;
 import be.maximvdw.placeholderapi.PlaceholderReplaceEvent;
@@ -10,14 +15,21 @@ import me.mafkees92.Main;
 public class mafkees_average_island_rate {
 
 	public mafkees_average_island_rate(Main plugin) {
+		
+	ASkyBlockAPI skyBlock = ASkyBlockAPI.getInstance();
 
 		PlaceholderAPI.registerPlaceholder(plugin, "mafkees_average_island_rate", new PlaceholderReplacer() {
 			@Override
 			public String onPlaceholderReplace(PlaceholderReplaceEvent event) {
 				Player player = event.getPlayer();
 				if (player != null) {
-					return me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(player, "%islandrate_average_rating%")
-							.substring(0, 1);
+					if(skyBlock.hasIsland(player.getUniqueId())) {
+						return me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(player, "%islandrate_average_rating%").substring(0, 1);
+					}
+					else if(skyBlock.inTeam(player.getUniqueId())) {
+						UUID IslandLeader = skyBlock.getTeamLeader(player.getUniqueId());
+						return me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(Bukkit.getPlayer(IslandLeader), "%islandrate_average_rating%").substring(0, 1);
+					}
 				}
 				return null;
 			}
