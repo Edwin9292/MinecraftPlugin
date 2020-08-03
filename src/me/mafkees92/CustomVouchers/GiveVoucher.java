b6d21c85124c51a4fe71e60c9e2e982711c60052
Mafkees92 <unconfigured@null.spigotmc.org>
2020-08-03 15:32:05 +0200

    edited givevoucher command and separted method for flyvouchers

--- src/me/mafkees92/CustomVouchers/GiveVoucher.java
+++ src/me/mafkees92/CustomVouchers/GiveVoucher.java
@@ -2,15 +2,20 @@ package me.mafkees92.CustomVouchers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import com.wasteofplastic.askyblock.util.Util;
 
 import me.mafkees92.Files.Messages;
 import me.mafkees92.Utils.Utils;
@@ -23,46 +28,72 @@ public class GiveVoucher implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!(sender instanceof Player))
 			return false;
-		Player player = (Player) sender;
+		Player commandExecutor = (Player) sender;
 
-		if (player.isOp() || player.hasPermission(permission)) {
-			if (args.length == 3) {
+		if (commandExecutor.isOp() || commandExecutor.hasPermission(permission)) {
+			if (args.length >= 2) {
 				Player targetPlayer = Bukkit.getPlayer(args[0]);
 
 				if (targetPlayer != null && targetPlayer.isOnline()) {
 					if (args[1].equalsIgnoreCase("permission")) {
-							
-						ItemStack item = new ItemStack(Material.PAPER);
-						ItemMeta meta = item.getItemMeta();
-						meta.setDisplayName(Utils.colorize("&bPermission Voucher"));
-						List<String> lore = new ArrayList<String>();
-						lore.add(Utils.colorize("&3Right Click to use this voucher"));
-						lore.add(Utils.colorize("&3This voucher will give you permission to use the /givecustompotion command"));
-						meta.setLore(lore);
-						item.setItemMeta(meta);
-						item = Utils.setNBTTag(item, "customVoucher", "permission");
-						item = Utils.setNBTTag(item, "permission", args[2]);
 						
-						if(targetPlayer.getInventory().firstEmpty() == -1) {
-							//inventory is full
-							player.sendMessage(Messages.CustomVoucherMessages.inventoryFull(targetPlayer));
+						switch (args[2]) {
+						case "essentials.fly":
+							if(args.length == 5) {
+								giveFlyVoucher(targetPlayer, args[2], args[3], args[4], commandExecutor);
+							}
+							else {
+								commandExecutor.sendMessage(Messages.invalidFlyVoucherArguments);
+							}
+							break;
+						default:
+							break;
 						}
-						else {
-							targetPlayer.getInventory().addItem(item);
-						}
-						return true;			
+						return true;
 					}
-					player.sendMessage(Messages.CustomVoucherMessages.invalidType);
+					commandExecutor.sendMessage(Messages.invalidVoucherType);
 					return true;
 				}
-				player.sendMessage(Messages.GeneralMessages.invalidTargetPlayer);
+				commandExecutor.sendMessage(Messages.invalidTargetPlayer);
 				return true;
 			}
-			player.sendMessage(Messages.CustomVoucherMessages.invalidArguments);
+			commandExecutor.sendMessage(Messages.invalidVoucherArguments);
 			return true;
 		}
-		player.sendMessage(Utils.colorize(Messages.GeneralMessages.noPermission));
+		commandExecutor.sendMessage(Messages.noPermission);
 		return true;
-
 	}
+	
+	private boolean giveFlyVoucher(Player targetPlayer, String permission, String duration, String stackable, Player commandExector) {
+		ItemStack item = new ItemStack(Material.FEATHER);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(Messages.flyVoucherName);
+		
+		List<String> lore = new ArrayList<String>();
+		lore.addAll(Messages.flyVoucherLore);
+        for (ListIterator<String> i = lore.listIterator(); i.hasNext(); )   {
+        	String loreLine = i.next().replace("%duration%", Utils.luckPermDurationToFullDuration(duration));
+        	loreLine = loreLine.replace("%rarity%", Utils.luckPermsDurationToRarityString(duration));
+            i.set(Util.colorize(loreLine));
+        }
+		
+		meta.setLore(lore);
+		meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		meta.addEnchant(Enchantment.FROST_WALKER, 1, false);
+		item.setItemMeta(meta);
+		item = Utils.setNBTTag(item, "customVoucher", "permission");
+		item = Utils.setNBTTag(item, "permission", permission);
+		item = Utils.setNBTTag(item, "duration", duration);
+		item = Utils.setNBTTag(item, "stackable", stackable);
+		
+		if(targetPlayer.getInventory().firstEmpty() == -1) {
+			//inventory is full
+			commandExector.sendMessage(Messages.inventoryFull(targetPlayer));
+		}
+		else {
+			targetPlayer.getInventory().addItem(item);
+		}
+		return true;			
+	}
+	
 }
