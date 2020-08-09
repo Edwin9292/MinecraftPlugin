d90816b2e2c2f886cb3de3b936e03357f95e6678
Mafkees92 <unconfigured@null.spigotmc.org>
2020-08-09 15:04:03 +0200

    created /setlore command

--- src/me/mafkees92/RenameItems/SetLore.java
+++ src/me/mafkees92/RenameItems/SetLore.java
@@ -1,6 +1,6 @@
 package me.mafkees92.RenameItems;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.command.Command;
@@ -10,24 +10,43 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class SetLore implements CommandExecutor{
+import me.mafkees92.Files.Messages;
+import me.mafkees92.Utils.Utils;
+
+public class SetLore implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
-		if(sender instanceof Player) {
-			Player player = (Player) sender;
+		if(!(sender instanceof Player)) 
+			return false;
 			
-			ItemStack item = player.getInventory().getItemInMainHand();
-			ItemMeta meta = item.getItemMeta();
-			
-			List<String> lore = new ArrayList<String>();
-			
-			
-			meta.setLore(lore);
-			
+		Player player = (Player) sender;
+		if(player.isOp() || player.hasPermission("mafkeesplugin.setlore")) {
+			if(args.length != 0){
+				String loreText = String.join(" ", args);
+				String[] loreLines = loreText.split("[/]");
+				if(loreLines.length > 0){
+					for (int i = 0; i < loreLines.length; i++) {
+						loreLines[i] = Utils.colorize("&f" + loreLines[i]);
+					}
+				}
+								
+				ItemStack item = player.getInventory().getItemInMainHand();
+				ItemMeta meta = item.getItemMeta();
+				List<String> lore = Arrays.asList(loreLines);
+				
+				meta.setLore(lore);
+				item.setItemMeta(meta);
+				
+				player.sendMessage(Messages.succeededSettingLore);
+				
+				return true;
+			}
+			player.sendMessage(Messages.noSetLoreArguments);
+			return true;
 		}
-		return false;
+		player.sendMessage(Messages.noPermission);
+		return true;
 	}
-
 }
