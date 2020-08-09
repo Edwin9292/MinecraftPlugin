611cf1694a9f3ba2f114924f2ed6961ff96a7f98
Mafkees92 <unconfigured@null.spigotmc.org>
2020-08-09 15:03:38 +0200

    added setlore command

--- src/me/mafkees92/Main.java
+++ src/me/mafkees92/Main.java
@@ -8,8 +8,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.wasteofplastic.askyblock.ASkyBlock;
 
 import me.mafkees92.ActionBar.ActionBar;
+import me.mafkees92.CustomHoppers.CustomHoppers;
 import me.mafkees92.CustomHoppers.GiveChunkHopper;
-import me.mafkees92.CustomHoppers.HopperData;
 import me.mafkees92.CustomHoppers.HopperEvents;
 import me.mafkees92.CustomPotions.CustomSplashPotions;
 import me.mafkees92.CustomPotions.GiveCustomPotion;
@@ -21,6 +21,7 @@ import me.mafkees92.Files.Messages;
 import me.mafkees92.HologramParkour.HologramParkour;
 import me.mafkees92.MVdWPlaceholders.mvdwPlaceholders;
 import me.mafkees92.RenameItems.RenameItem;
+import me.mafkees92.RenameItems.SetLore;
 import me.mafkees92.VoidTeleportation.VoidDamage;
 import net.luckperms.api.LuckPerms;
 import net.milkbowl.vault.economy.Economy;
@@ -29,26 +30,26 @@ public class Main extends JavaPlugin {
 
 	private LuckPerms luckperms;
 	private boolean shopGuiPlusApiEnabled = false;
-    public static Economy econ = null;
-	private HopperData data;
+	public static Economy econ = null;
+	private CustomHoppers customHoppers;
 
 	public void onEnable() {
 
 		// saveDefaultConfig();
 		new Messages(this, "Messages.yml");
-		data = new HopperData(this, "HopperData.yml");
+		customHoppers = new CustomHoppers(this);
 
 		getServer().getPluginManager().registerEvents(new VoidDamage(this), this);
 		getServer().getPluginManager().registerEvents(new CustomSplashPotions(this), this);
 		getServer().getPluginManager().registerEvents(new ActionBar(this), this);
 		getServer().getPluginManager().registerEvents(new VoucherUsageEvent(this), this);
-		getServer().getPluginManager().registerEvents(new HopperEvents(this), this);
+		getServer().getPluginManager().registerEvents(new HopperEvents(customHoppers), this);
 		getCommand("rename").setExecutor(new RenameItem());
+		getCommand("setlore").setExecutor(new SetLore());
 		getCommand("givecustompotion").setExecutor(new GiveCustomPotion());
 		getCommand("givevoucher").setExecutor(new GiveVoucher());
 		getCommand("flytime").setExecutor(new GetRemainingFlyTime(this));
 		getCommand("givecustomhopper").setExecutor(new GiveChunkHopper(this));
-		new HologramParkour(this);
 
 		if (getServer().getPluginManager().isPluginEnabled("MVdWPlaceholderAPI")) {
 			new mvdwPlaceholders(this);
@@ -65,29 +66,41 @@ public class Main extends JavaPlugin {
 			Bukkit.getLogger().info("Hooked into ShopGUI+");
 			shopGuiPlusApiEnabled = true;
 		}
-		
-		if(!setupEconomy()) {
+
+		if (!setupEconomy()) {
 			Bukkit.getLogger().warning(ChatColor.DARK_RED + "ERROR: could not load vault plugin");
 		}
+		if (Bukkit.getPluginManager().getPlugin("ProtocolLib") == null) {
+			System.out.println("Timeout > You need ProtocolLib in order to use this plugin");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
+		new HologramParkour(this);
+
+	}
+
+	public void onDisable() {
+		if (customHoppers != null)
+			customHoppers.onDisable();
 	}
 
 	private boolean setupEconomy() {
-        RegisteredServiceProvider<Economy> economyProvider = ASkyBlock.getPlugin().getServer().getServicesManager()
-                .getRegistration(net.milkbowl.vault.economy.Economy.class);
-        if (economyProvider != null) {
-            econ = economyProvider.getProvider();
-        }
-        return econ != null;
+		RegisteredServiceProvider<Economy> economyProvider = ASkyBlock.getPlugin().getServer().getServicesManager()
+				.getRegistration(net.milkbowl.vault.economy.Economy.class);
+		if (economyProvider != null) {
+			econ = economyProvider.getProvider();
+		}
+		return econ != null;
 	}
 
 	public LuckPerms getLuckperms() {
 		return luckperms;
 	}
 
-	public HopperData getHopperData() {
-		return data;
+	public CustomHoppers getCustomHoppers() {
+		return customHoppers;
 	}
-	
+
 	public boolean isShopGuiPlusEnabled() {
 		return shopGuiPlusApiEnabled;
 	}
