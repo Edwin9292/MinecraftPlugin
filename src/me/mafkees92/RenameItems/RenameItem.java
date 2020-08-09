40f99698966cf4d242a37746b87aa7740d155b2d
Mafkees92 <unconfigured@null.spigotmc.org>
2020-08-09 15:03:54 +0200

    added permissions check for the command

--- src/me/mafkees92/RenameItems/RenameItem.java
+++ src/me/mafkees92/RenameItems/RenameItem.java
@@ -8,32 +8,36 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import me.mafkees92.Files.Messages;
 import me.mafkees92.Utils.Utils;
 
-public class RenameItem implements CommandExecutor{
+public class RenameItem implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(!(sender instanceof Player)) return false;
-		Player player = (Player) sender;
+		if (!(sender instanceof Player))
+			return false;
 		
-		if(args.length > 1) {
+		Player player = (Player) sender;		
+		if (player.isOp() || player.hasPermission("mafkeesplugin.rename")) {
+			if (args.length != 0) {
+				ItemStack item = player.getInventory().getItemInMainHand();
+				
+				if (item != null && item.getType() != Material.AIR) {
+					String displayName = String.join(" ", args);
+					ItemMeta meta = item.getItemMeta();
+					meta.setDisplayName(Utils.colorize("&f" + displayName));
+					item.setItemMeta(meta);
+		
+					return true;
+				}
+				player.sendMessage(Utils.colorize("&cYou must hold an item in your hand when using this command"));
+				return true;
+			} 
 			player.sendMessage(Utils.colorize("&cInvalid arguments. Try /rename newName"));
-			return false;
-		}
-		
-		ItemStack item = player.getInventory().getItemInMainHand();
-		if(item == null || item.getType() == Material.AIR) {
-			player.sendMessage(Utils.colorize("&cYou must hold an item in your hand when using this command"));
-			return false;
-		}
-		
-		ItemMeta meta = item.getItemMeta();
-		meta.setDisplayName(Utils.colorize("&f" + args[0]).replace("_", " "));
-		item.setItemMeta(meta);
-		
-		
-		
+			return true;
+		} 
+		player.sendMessage(Messages.noPermission);
 		return true;
 	}
 }
