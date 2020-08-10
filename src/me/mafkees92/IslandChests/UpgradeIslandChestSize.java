f768f9885203b382f16f5a59d275e6b910df2b3f
Mafkees92 <unconfigured@null.spigotmc.org>
2020-08-10 15:20:05 +0200

    Create UpgradeIslandChestSize.java

---
+++ src/me/mafkees92/IslandChests/UpgradeIslandChestSize.java
@@ -0,0 +1,49 @@
+package me.mafkees92.IslandChests;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.mafkees92.Main;
+import me.mafkees92.Files.IslandChest;
+import me.mafkees92.Files.Messages;
+import me.mafkees92.Utils.Utils;
+
+public class UpgradeIslandChestSize implements CommandExecutor
+{
+
+	Main plugin; 
+	
+	public UpgradeIslandChestSize(Main plugin) {
+		this.plugin = plugin;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] args) {
+		
+		if(sender instanceof Player) {
+			sender.sendMessage(Messages.noPermission);
+			return true;
+		}
+		
+		if(args.length == 1) {
+			if(Bukkit.getOfflinePlayer(args[0]) != null) {
+				UUID islandOwner = Utils.getTeamOrIslandOwner(Bukkit.getOfflinePlayer(args[0]).getUniqueId());
+				IslandChest chest = plugin.getIslandChests().getIslandChest(islandOwner);
+				if(chest.setInventorySize(chest.getInventory().getSize() + 9)) {
+					sender.sendMessage("Succesfully increased the size of the islandChest to " + chest.getInventory().getSize());
+				}
+				else {
+					sender.sendMessage("Players island chest is already on max size");
+				}
+			}
+		}
+		return false;
+	}
+
+}
