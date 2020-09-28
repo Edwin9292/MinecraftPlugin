a2b68b65ddb10d4ce65ddf579b17099a8262e5e5
Mafkees92 <unconfigured@null.spigotmc.org>
2020-09-28 15:51:44 +0200

    made the voidteleport teleport you to the home of the island/spawn you are on

--- src/me/mafkees92/VoidTeleportation/VoidDamage.java
+++ src/me/mafkees92/VoidTeleportation/VoidDamage.java
@@ -1,6 +1,7 @@
 package me.mafkees92.VoidTeleportation;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,6 +10,7 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.plugin.Plugin;
 
 import com.wasteofplastic.askyblock.ASkyBlockAPI;
+import com.wasteofplastic.askyblock.Island;
 
 import me.mafkees92.Main;
 
@@ -27,17 +29,34 @@ public class VoidDamage implements Listener {
 		if (e.getEntity() instanceof Player) {
 			Player player = (Player) e.getEntity();
 			String worldName = player.getWorld().getName();
-			if (worldName.equalsIgnoreCase("staff") || worldName.equalsIgnoreCase("skyblock")
+			if (worldName.equalsIgnoreCase("staff") 
+					|| worldName.equalsIgnoreCase("skyblock")
 					|| worldName.equalsIgnoreCase("skyblock_nether")
 					|| worldName.equalsIgnoreCase("skyblock_the_end")
 					|| worldName.equalsIgnoreCase("testing")) {
 				e.setCancelled(true);
+				/*
 				if(ASkyBlockAPI.getInstance().hasIsland(player.getUniqueId()) || ASkyBlockAPI.getInstance().inTeam(player.getUniqueId())) {
 					player.teleport(ASkyBlockAPI.getInstance().getHomeLocation(player.getUniqueId()));
 				}
 				else{
 					Bukkit.dispatchCommand(player, "spawn");
 				}
+				*/
+				Island is = ASkyBlockAPI.getInstance().getIslandAt(player.getLocation());
+				if(is != null) {
+					if(is.isSpawn()) {
+						player.teleport(is.getSpawnPoint().clone().add(0.5, 0, 0.5));
+					}
+					else {
+						Location loc = ASkyBlockAPI.getInstance().getHomeLocation(is.getOwner());
+						player.teleport(loc.clone().add(0.5,0,0.5));
+					}
+				}
+				else {
+					Bukkit.dispatchCommand(player, "spawn");
+				}
+
 				player.setFallDistance(0);
 			}
 		}
