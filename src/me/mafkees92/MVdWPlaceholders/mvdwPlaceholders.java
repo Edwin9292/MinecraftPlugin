b1d650949ae82ddfa06bb7737669510e1873369a
Mafkees92 <unconfigured@null.spigotmc.org>
2020-10-13 22:48:19 +0200

    fixed naming of placeholder classes

--- src/me/mafkees92/MVdWPlaceholders/mvdwPlaceholders.java
+++ src/me/mafkees92/MVdWPlaceholders/mvdwPlaceholders.java
@@ -8,15 +8,11 @@ public class mvdwPlaceholders {
 
 		//register all custom placeholders here
 		new mafkees_island_size(plugin);
-		new mafkees_average_island_rate(plugin);
+		new mafkees_island_chest_size(plugin);
 		new mafkees_player_health(plugin);
 		new mafkees_remaining_fly_time(plugin);
-		new mafkees_island_chest_size(plugin);
+		new mafkees_average_island_rate(plugin);
 		new mafkees_max_team_members(plugin);
 		new mafkees_island_locked(plugin);
 	}
-	
-	
-	
-	
 }
