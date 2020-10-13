65ea5a13e37f21aaebd2721f14f2616b52389661
Mafkees92 <unconfigured@null.spigotmc.org>
2020-10-13 22:47:28 +0200

    tightened code

--- src/me/mafkees92/MVdWPlaceholders/mafkees_average_island_rate.java
+++ src/me/mafkees92/MVdWPlaceholders/mafkees_average_island_rate.java
@@ -2,31 +2,23 @@ package me.mafkees92.MVdWPlaceholders;
 
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import be.maximvdw.placeholderapi.PlaceholderAPI;
 import me.mafkees92.Main;
-import me.mafkees92.Files.IslandChest;
 import me.mafkees92.Utils.Utils;
 
 public class mafkees_average_island_rate {
 
 	public mafkees_average_island_rate(Main plugin) {
 		
-		PlaceholderAPI.registerPlaceholder(plugin, "mafkees_island_chest_size", event -> {
+		PlaceholderAPI.registerPlaceholder(plugin, "mafkees_average_island_rate", event -> {
 			Player player = event.getPlayer();
 			if (player != null) {
-				UUID islandOwner = Utils.getTeamOrIslandOwner(player.getUniqueId());
-
-				if(islandOwner == null)
-					return Utils.colorize("&cNo Island Found");
-
-				else {
-					IslandChest chest = plugin.getIslandChests().getIslandChest(islandOwner);
-					if(chest != null) {
-						return "" + chest.getInventory().getSize();
-					}
-					return "Error";
+				UUID islandLeader = Utils.getTeamOrIslandOwner(player.getUniqueId());
+				if(islandLeader != null) {
+					return me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(Bukkit.getPlayer(islandLeader), "%islandrate_average_rating%").substring(0, 1);
 				}
 			}
 			return null;
