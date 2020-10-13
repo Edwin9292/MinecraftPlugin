a75150561d12f984ccb1dc333c9aac9da261129a
Mafkees92 <unconfigured@null.spigotmc.org>
2020-10-13 13:42:00 +0200

    command added to override /rules

---
+++ src/me/mafkees92/Commands/Rules.java
@@ -0,0 +1,24 @@
+package me.mafkees92.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.mafkees92.Files.Messages;
+
+public class Rules implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] arg3) {
+		if(!(sender instanceof Player)) 
+			return false;
+		
+		Player player = (Player) sender;
+		
+		player.sendMessage(Messages.rulesMessage.toArray(new String[0]));
+		
+		return true;
+	}
+
+}
